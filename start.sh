5f56dd358c08f20d15df2558a31ebd11af4b9b32
Juan López Rubio <j.lopez.r@gmail.com>
2017-11-17 12:12:07 +0100

    Change port from 80 to 6080

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -it --rm -p 6080:80 -p 5900:5900 -v $PWD/data:/data --privileged -e RESOLUTION=$RESOLUTION hub.aaaida.com:5000/flowit/desktop
+docker run -it --rm -p 6080:6080 -p 5900:5900 -v $PWD/data:/data --privileged -e RESOLUTION=$RESOLUTION hub.aaaida.com:5000/flowit/desktop
